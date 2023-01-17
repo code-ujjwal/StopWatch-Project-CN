a68b46c562a4a0f78bc439ea7151c9388f57ca2d
Ujjwal Kant <115913132+code-ujjwal@users.noreply.github.com>
2023-01-18 00:10:34 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ var min = 0;
 var sec = 0;
 var stoptime = true;
 
+/* Code to start the stopwatch*/
+
 function startTimer() {
 	if (stoptime == true) {
 		stoptime = false;
@@ -11,11 +13,15 @@ function startTimer() {
 	}
 }
 
+/* Code to stop the stopwatch*/
+
 function stopTimer() {
 	if (stoptime == false) {
 		stoptime = true;
 	}
 }
+
+/* function code to display hours, minutes, seconds on the stop watch*/
 
 function timerCycle() {
 	if (stoptime == false) {
@@ -26,14 +32,15 @@ function timerCycle() {
 		sec = sec + 1;
 
 		if (sec == 60) {
-			min = min + 1;
+			min = min + 1; /* minute increase by one after 60 sec */
 			sec = 0;
 		}
 		if (min == 60) {
-			hr = hr + 1;
+			hr = hr + 1; /* hour increase by one after 60 min */
 			min = 0;
 			sec = 0;
 		}
+		/* Code to display 0 to those number which are less than 10  */
 		if (sec < 10) {
 			sec = "0" + sec;
 		}
@@ -48,7 +55,10 @@ function timerCycle() {
 		setTimeout("timerCycle()", 1000);
 	}
 }
-function resetTimer(){
+
+/* code to reset timer in stopwatch */
+
+function resetTimer(){   /* by this function we can reset time in our stopwatch */
 	timer.innerHTML = "00:00:00";
 	stoptime = true;
 	hr = 0;
